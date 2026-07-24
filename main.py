7c5ee8b0debfee9af89f904742eaa5ccbcda3f88
portAmmar <ammar.k.alhussary@proton.me>
2026-07-24 13:18:11 +0300

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello":"world"}
+    return {
+            "name":"Task API"
+            "version":"1.0"
+            "enpoints":["/tasks"]
+            }
+
+@app.get("health")
+def test_server_health():
+    return {"status": "ok"}
