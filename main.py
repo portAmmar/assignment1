aeffe107bd221e74369aaffdf5f889ea6f11894c
portAmmar <ammar.k.alhussary@proton.me>
2026-07-24 14:20:29 +0300

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+tasks = [
+        {
+                "id" : 1,
+                "title" : "first task",
+                "done" : false
+
+            },{
+                "id" : 2,
+                "title" : "task 2",
+                "done" : false
+            },{
+                "id" : 3,
+                "title" : "task 3"
+                "done" : false
+            }
+            ]
+
 @app.get("/")
 def read_root():
     return {
@@ -13,3 +30,19 @@ def read_root():
 @app.get("health")
 def test_server_health():
     return {"status": "ok"}
+
+@app.get("/tasks")
+def get_all_tasks():
+    return tasks
+
+@app.get("/task/{task_id}")
+def get_task_by_id(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+    return { "error": "Task 99 not found" }
+
+
+
+
